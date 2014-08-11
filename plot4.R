9d4f961badfd3a7c71101bf63600d5def0cb48dc
Regene <regenelee.miao@gmail.com>
2014-08-11 16:14:32 +0800

    Update plot4.R

--- plot4.R
+++ plot4.R
@@ -14,8 +14,11 @@ par(mfrow=c(2,2), mar=c(4,4,1,1))
 
     plot(datatime,Global_active_power,type="l",
          ylab="Global Active Power", xlab="")
+    axis(1,c(datatime[1],mean(datatime),datatime[2880]),labels=c("Thu","Fri","Sat"))
+
     plot(datatime, Voltage, type="l",
-         ylab="Voltage", xlab="datatime")
+         ylab="Voltage", xlab="datetime")
+
     plot(datatime, Sub_metering_1, type="l",
          ylab="Energy Sub metering", xlab="")
     lines(datatime,Sub_metering_2, col='Red')
@@ -23,6 +26,11 @@ par(mfrow=c(2,2), mar=c(4,4,1,1))
     legend("topright",col=c("Black","Red","Blue"), 
            legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"), 
            lty=1, lwd=2, bty="n")
-    plot(datatime, Global_reactive_power, type="l", xlab="datatime")
+    axis(1,c(datatime[1],mean(datatime),datatime[2880]),labels=c("Thu","Fri","Sat"))
+
+    plot(datatime, Global_reactive_power, type="l", xlab="datetime")
+    axis(1,c(datatime[1],mean(datatime),datatime[2880]),labels=c("Thu","Fri","Sat"))
 
 dev.off()
+
+
