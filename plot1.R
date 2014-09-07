fc18d6f45f053dae4093b232f913137149897d25
Regene <regenelee.miao@gmail.com>
2014-09-07 22:08:34 +0800

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,21 +1,53 @@
-# First, deal with the "Date". The as.Date function is only useful for character
-Date <- as.character(Date)
-Date <- as.Date(Date,"%d/%m/%Y")
-startdate <- as.Date("2017-02-01")
-enddate <- as.Date("2017-02-03")
-my_data0 <- my_data[which(my_data$Date >= startdate &
-                             my_data$Date <= enddate),]
-# plot1
-my_data0[,3] <- as.vector(my_data0[,3]) # this step cannot be forget
-my_data0[,3] <- as.numeric(my_data0[,3])
-x1 <- my_data0[,3]
-# compute the range of x1, which help determine the "breaks"
-# in hist function
-range(x1)
-b <- c(0,0.5,1,1.5,2,2.5,3,3.5,4,4.5,5,5.5,6,6.5,7,7.5,8)
-# draw the plot
-png("plot1.png",width=480,height=480)
-hist(x1,breaks=b,freq=TRUE,col="red",
+library(data.table)
+library(parallel)
+library(lubridate)
+dataFN <- fread("household_power_consumption.txt",
+                colClasses="character",skip = 66637, nrows = 2880)
+ColNames <- c("Date", "Time", "Global_active_power" ,
+              "Global_reactive_power", "Voltage", "Global_intensity",
+              "Sub_metering_1", "Sub_metering_2", "Sub_metering_3")
+Consump <- dataFN
+setnames(Consump,ColNames)
+######################################################################
+## Converting the data to easy handle
+
+## Change the type of data and time into POSIXct & POSIXt
+Consump$myDate <- paste(Consump$Date,Consump$Time)
+Consump$myDate <- dmy_hms(Consump$myDate)
+## OR
+Consump$myDate <- striptime(Consump$myDate,"%d/%m/%Y %H:%M:%S")
+
+## Change the class of data
+Consump$Global_active_power <-
+  sapply(Consump$Global_active_power, as.numeric)
+
+Consump$Global_reactive_power <-
+  sapply(Consump$Global_reactive_power, as.numeric)
+
+Consump$Voltage <-
+  sapply(Consump$Voltage, as.numeric)
+
+Consump$Global_intensity <-
+  sapply(Consump$Global_intensity, as.numeric)
+
+Consump$Sub_metering_1 <- sapply(Consump$Sub_metering_1,
+                                       as.numeric)
+
+Consump$Sub_metering_2 <- sapply(Consump$Sub_metering_2,
+                                       as.numeric)
+
+Consump$Sub_metering_3 <- sapply(Consump$Sub_metering_3,
+                                       as.numeric)
+
+######################################################################
+## Plot code:
+png("plot1.png",width=480,height=480,unit="px",pointsize=12,bg="transparent")
+hist(Consump$Global_active_power, col = "red",
      main="Global Active Power",
-     xlab = "Global Active Power (kilowatts)")
+     xlab="Global Active Power (kilowatts)")
 dev.off()
+## Or use the ggplot2 package
+library(ggplot2)
+qplot(Global_active_power, data = Consump, geom = "histogram",
+      binwidth = 0.5, fill = "red", colour = "black")
+
